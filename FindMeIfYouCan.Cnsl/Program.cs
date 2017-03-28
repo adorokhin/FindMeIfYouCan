8d70184d3b847c1b08f0e38761ea5ace249f4102
Alex Dorokhin <adorokhin@hotmail.com>
2017-03-28 09:21:29 -0400

    Lib added

---
+++ FindMeIfYouCan.Cnsl/Program.cs
@@ -0,0 +1,17 @@
+﻿using FindMeIfYouCan.Lib;
+using System;
+
+//C:\Users\AlexD>set | findstr "\<Path\>"
+
+
+namespace FindMeIfYouCan.Cnsl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SecretClass.password);
+            Console.ReadLine();
+        }
+    }
+}
